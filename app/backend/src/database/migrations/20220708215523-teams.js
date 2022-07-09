0b7d50480e3e304d1fef04de61844fcadf0af86e
Alex Silva <alexcssilva@gmail.com>
2022-07-08 22:34:05 -0300

    feat: added migrations-teams

---
+++ app/backend/src/database/migrations/20220708215523-teams.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      team_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        underscored: true,
+      },
+    })
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.createTable('teams');
+  }
+};
