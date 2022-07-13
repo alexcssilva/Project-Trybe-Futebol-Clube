dc4be6d131478ac900116150ff87f85edc57202d
Alex Silva <alexcssilva@gmail.com>
2022-07-13 17:47:48 -0300

    feat: added validatePassword/routes - Desafio 07

--- app/backend/src/router/user/user.routes.ts
+++ app/backend/src/router/user/user.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { validateEmail } from '../../middleware/validateLogin';
+import { validateEmail, validatePassword } from '../../middleware/validateLogin';
 import { UserFactory } from '../../factory'
 
 const userRouter = Router();
 
-userRouter.post('/login', validateEmail, (req, res, next) => {
+userRouter.post('/login', validateEmail, validatePassword, (req, res, next) => {
   UserFactory().login(req, res, next);
 });
 
