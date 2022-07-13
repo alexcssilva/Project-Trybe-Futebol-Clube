7b38db946346dc0fed7cc70da58d877f42f913d2
Alex Silva <alexcssilva@gmail.com>
2022-07-12 22:19:26 -0300

    feat: added user.controller

---
+++ app/backend/src/controllers/user.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import { IUserService } from '../protocols';
+
+class Controller {
+  constructor(private service: IUserService) {
+    this.service = service;
+  };
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email, password } = req.body;
+      console.log('; req.body', req.body);
+      const token = await this.service.login(email, password);
+      return res.status(200).json({ token })
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default Controller;
