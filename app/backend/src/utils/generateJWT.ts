6837a02e85100c7e9ca70c71882119f6c79396a7
Alex Silva <alexcssilva@gmail.com>
2022-07-12 22:23:39 -0300

    feat: added generateJWT

---
+++ app/backend/src/utils/generateJWT.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import * as jwt from 'jsonwebtoken';
+import { IUser } from '../protocols';
+
+function generateJWT(payload: Omit<IUser, 'password'>) {
+  const secret = process.env.JWT_SECRET || 'jwt_secret';
+
+  const jwtConfig: object = {
+    expiresIn: '10h',
+    algorithm: 'HS256',
+  };
+
+  const token = jwt.sign({ payload }, secret, jwtConfig);
+
+  return token;
+}
+
+export default generateJWT;
