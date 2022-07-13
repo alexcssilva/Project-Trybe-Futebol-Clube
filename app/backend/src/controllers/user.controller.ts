dd76835452469c1352acf153cf9e2b28eb62f400
Alex Silva <alexcssilva@gmail.com>
2022-07-13 18:54:56 -0300

    feat: update user.controller - Desafio 09

--- app/backend/src/controllers/user.controller.ts
+++ app/backend/src/controllers/user.controller.ts
@@ -9,8 +9,8 @@ class Controller {
   async login(req: Request, res: Response, next: NextFunction) {
     try {
       const { email, password } = req.body;
-
       const token = await this.service.login(email, password);
+      if (!token) return res.status(401).json({ message: 'Incorrect email or password'});
       return res.status(200).json({ token })
     } catch (error) {
       next(error);
--- app/backend/src/services/users.service.ts
+++ app/backend/src/services/users.service.ts
@@ -2,7 +2,6 @@ import * as bcrypt from 'bcryptjs';
 import generateJWT from '../utils/generateJWT';
 import { IUserService, IUserModel } from '../protocols';
 
-
 class User implements IUserService {
   constructor(private model: IUserModel) {
     this.model = model;
@@ -10,7 +9,7 @@ class User implements IUserService {
 
   async login(email: string, password: string): Promise<string | boolean> {
       const user = await this.model.findUser(email);
-
+      
       if (!user) return false;
 
       const check = bcrypt.compareSync(password, user.password);
