8a0126a4da1477f6499386ca9f4b038d9c7579f1
Alex Silva <alexcssilva@gmail.com>
2022-07-14 22:32:53 -0300

    feat: added team.routesy - Desafio 15

---
+++ app/backend/src/router/team.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { TeamFactory } from '../factory/team.factory'
+
+const teamRouter = Router();
+
+teamRouter.get('/teams', (req, res, next) => {
+  TeamFactory().findAll(req, res, next);
+});
+
+export default teamRouter;
--- app/backend/src/router/user.routes.ts
+++ app/backend/src/router/user.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { validateEmail, validatePassword } from '../../middleware/validateLogin';
-import { UserFactory } from '../../factory'
+import { validateEmail, validatePassword } from '../middleware/validateLogin';
+import { UserFactory } from '../factory/user.factory'
 
 const userRouter = Router();
 
