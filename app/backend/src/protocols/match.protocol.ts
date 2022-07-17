cedb0adc5f230656dcd4cae5d1306c30dc8ce8d6
Alex Silva <alexcssilva@gmail.com>
2022-07-17 19:23:23 -0300

    feat: update match.protocol and team.protocol  - Desafio 25, 26, 27 and 28

--- app/backend/src/protocols/match.protocol.ts
+++ app/backend/src/protocols/match.protocol.ts
@@ -5,17 +5,18 @@ export interface IMatch {
   awayTeam: number;
   awayTeamGoals: number;
   inProgress: boolean;
-};
+}
 
 export interface IMatchModel {
   findAll(inProgress: boolean | null): Promise<IMatch[] | null>;
   createMatch(match: IMatch): Promise<IMatch>;
   updatePatch(id: string): Promise<unknown>;
-};
+  scoreUpdate(homeTeamGoals: number, awayTeamGoals: number, id: string): Promise<unknown>;
+}
 
 export interface IMatchService {
   findAll(inProgress: boolean | null): Promise<IMatch[] | null>;
   updateMatch(match: IMatch): Promise<IMatch>;
   updatePatch(id: string): Promise<unknown>;
-};
-
+  scoreUpdate(homeTeamGoals: number, awayTeamGoals: number, id: string): Promise<unknown>;
+}
--- app/backend/src/protocols/team.protocol.ts
+++ app/backend/src/protocols/team.protocol.ts
@@ -1,15 +1,15 @@
 export interface ITeam {
   id?: number;
   teamName: string;
-};
+}
 
 export interface ITeamModel {
   findAll(): Promise<ITeam[] | null>;
   findTeam(id: string): Promise<ITeam | null>;
-};
+  findTeamId(homeTeam: number, awayTeam: number): Promise<ITeam[] | null>;
+}
 
 export interface ITeamService {
   findAll(): Promise<ITeam[] | null>;
   findTeam(id: string): Promise<ITeam | null>;
-};
-
+}
