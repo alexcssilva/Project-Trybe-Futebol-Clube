5b3d2a52cadd4b311e68ac05cae1c6c588348f3f
Alex Silva <alexcssilva@gmail.com>
2022-07-16 23:57:33 -0300

    feat: update match.routes - Desafio 23 and 27

--- app/backend/src/router/match.routes.ts
+++ app/backend/src/router/match.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import validateJWT from '../middleware/validateJWT';
 import { MatchFactory } from '../factory/match.factory';
 
 const matchRouter = Router();
 
 matchRouter.get('/matches', (req, res, next) => {
   MatchFactory().findAll(req, res, next);
+});
+
+matchRouter.post('/matches', validateJWT, (req, res, next) => {
+  MatchFactory().createMatch(req, res, next);
 });
 
 export default matchRouter;
