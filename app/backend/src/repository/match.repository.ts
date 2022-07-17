442e8c4de97ede8ebaf8522694f7127efdfbb6cf
Alex Silva <alexcssilva@gmail.com>
2022-07-17 19:23:56 -0300

    feat: update match.repository and team.repository - Desafio 25, 26, 27 and 28

--- app/backend/src/repository/match.repository.ts
+++ app/backend/src/repository/match.repository.ts
@@ -2,32 +2,32 @@ import Model from '../database/models/match.model';
 import { IMatch, IMatchModel } from '../protocols/match.protocol';
 import teamModel from '../database/models/team.model';
 
-class MatchRepository implements IMatchModel{
-  constructor(private model = Model){
+class MatchRepository implements IMatchModel {
+  constructor(private model = Model) {
     this.model = model;
-  };
+  }
 
   findAll = async (inProgress: boolean | null): Promise<IMatch[] | null> => {
-    
+
     let matches = undefined;
 
     if (inProgress === null) {
-       matches = await this.model.findAll({ include: [
-        { model: teamModel, as: 'teamHome', attributes: { exclude: ['id'] }},
-        { model: teamModel, as: 'teamAway', attributes: { exclude: ['id'] }},
+      matches = await this.model.findAll({ include: [
+        { model: teamModel, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: teamModel, as: 'teamAway', attributes: { exclude: ['id'] } },
        ] });
     } else {
       matches = await this.model.findAll({ where: { inProgress }, include: [
-        { model: teamModel, as: 'teamHome', attributes: { exclude: ['id'] }},
-        { model: teamModel, as: 'teamAway', attributes: { exclude: ['id'] }},
-      ]});
+        { model: teamModel, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: teamModel, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ] });
     }
     return matches as IMatch[];
-  }
+  };
 
   createMatch = async (match: IMatch): Promise<IMatch> => {
     const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = match;
-    
+
     const matches = await this.model.create({
       homeTeam,
       awayTeam,
@@ -37,13 +37,25 @@ class MatchRepository implements IMatchModel{
     });
 
     return matches;
-  }
+  };
 
   updatePatch = async (id: string): Promise<unknown> => {
-    const match = await this.model.update({ inProgress: false }, { where: { id }})
+    const match = await this.model.update({ inProgress: false }, { where: { id } });
 
     return match;
-  }
-};
+  };
+
+  scoreUpdate = async (
+    homeTeamGoals: number,
+    awayTeamGoals: number,
+    id: string): Promise<unknown> => {
+    const updateMatch = await this.model.update({
+      homeTeamGoals,
+      awayTeamGoals,
+      id }, { where: { id } });
+
+    return updateMatch;
+  };
+}
 
 export default MatchRepository;
--- app/backend/src/repository/team.repository.ts
+++ app/backend/src/repository/team.repository.ts
@@ -1,22 +1,28 @@
 import Model from '../database/models/team.model';
 import { ITeam, ITeamModel } from '../protocols/team.protocol';
 
-class TeamRepository implements ITeamModel{
-  constructor(private model = Model){
+class TeamRepository implements ITeamModel {
+  constructor(private model = Model) {
     this.model = model;
-  };
+  }
 
   findAll = async (): Promise<ITeam[] | null> => {
     const teams = await this.model.findAll();
 
     return teams;
-  }
+  };
 
   findTeam = async (id: string): Promise<ITeam | null> => {
-    const team = await this.model.findOne({ where: { id }});
+    const team = await this.model.findOne({ where: { id } });
 
     return team;
-  }
-};
+  };
+
+  findTeamId = async (homeTeam: number, awayTeam: number): Promise<ITeam[] | null> => {
+    const teams = await this.model.findAll({ where: { id: [homeTeam, awayTeam] } });
+
+    return teams;
+  };
+}
 
 export default TeamRepository;
