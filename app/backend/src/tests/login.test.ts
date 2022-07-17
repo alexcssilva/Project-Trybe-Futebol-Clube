c2d045d0a20b75d26a1df70ee528695cae378a92
Alex Silva <alexcssilva@gmail.com>
2022-07-16 23:55:25 -0300

    fix: update login.test

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -14,10 +14,10 @@ describe('Test login', () => {
     sinon
       .stub(User, "findOne")
       .resolves({
-        id: 1,
-        username: 'Alex',
+        id: 2,
+        username: 'User',
         role: 'user',
-        email: 'test@trybe.com',
+        email: 'user@user.com',
         password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
       } as User);
   });
@@ -27,14 +27,14 @@ describe('Test login', () => {
   })
 
   const login = {
-    email: 'test@trybe.com',
-    password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
+    email: 'user@user.com',
+    password: 'secret_user',
   }
 
   it('se é possivel acessar com a validação do token', async () => {
     const response = await chai.request(app).post('/login').send(login);
 
-    expect(response.status).to.be.equal(201);
+    expect(response.status).to.be.equal(200);
     expect(response.body).have.property('token');
   });
 });
