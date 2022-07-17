e555391a44ff5acac9aac3fc5f9ccbfcb76f2634
Alex Silva <alexcssilva@gmail.com>
2022-07-17 17:08:37 -0300

    feat: update match.test

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -13,7 +13,7 @@ describe('Test match', () => {
    before(async () => {
     sinon
       .stub(Match, 'findOne')
-      .resolves([{
+      .resolves({
         id: 41,
         homeTeam: 16,
         homeTeamGoals: 2,
@@ -26,7 +26,7 @@ describe('Test match', () => {
         teamAway: {
           teamName: 'Internacional',
         }
-      }] as unknown as Match);
+      } as unknown as Match);
   });
 
   after(()=>{
@@ -48,56 +48,57 @@ describe('Test match', () => {
     }
   }
 
-  it('se é possivel buscar por um placar', async () => {
+  it('se é possivel criar um placar', async () => {
     const response = await chai.request(app).get('/matches').send(match);
 
     expect(response.status).to.be.equal(200);
   });
+
+  describe('Test match placares', () => {
+     before(async () => {
+      sinon
+        .stub(Match, 'findAll')
+        .resolves([{
+          id: 41,
+          homeTeam: 16,
+          homeTeamGoals: 2,
+          awayTeam: 9,
+          awayTeamGoals: 0,
+          inProgress: true,
+          teamHome: {
+            teamName: 'São Paulo',
+          },
+          teamAway: {
+            teamName: 'Internacional',
+          }
+        }] as unknown as Match[]);
+    });
+  
+    after(()=>{
+      (Match.findAll as sinon.SinonStub).restore();
+    })
+  
+    const matches = [{
+      id: 41,
+      homeTeam: 16,
+      homeTeamGoals: 2,
+      awayTeam: 9,
+      awayTeamGoals: 0,
+      inProgress: true,
+      teamHome: {
+        teamName: 'São Paulo',
+      },
+      teamAway: {
+        teamName: 'Internacional'
+      }
+    }]
+  
+    it('se o corpo de times é um array de objetos', async () => {
+      const response = await chai.request(app).get('/matches');
+  
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.be.a('array')
+    });
+  });
 });
 
-describe('Test match placares', () => {
-   before(async () => {
-    sinon
-      .stub(Match, 'findAll')
-      .resolves([{
-        id: 41,
-        homeTeam: 16,
-        homeTeamGoals: 2,
-        awayTeam: 9,
-        awayTeamGoals: 0,
-        inProgress: true,
-        teamHome: {
-          teamName: 'São Paulo',
-        },
-        teamAway: {
-          teamName: 'Internacional',
-        }
-      }] as unknown as Match[]);
-  });
-
-  after(()=>{
-    (Match.findAll as sinon.SinonStub).restore();
-  })
-
-  const matches = [{
-    id: 41,
-    homeTeam: 16,
-    homeTeamGoals: 2,
-    awayTeam: 9,
-    awayTeamGoals: 0,
-    inProgress: true,
-    teamHome: {
-      teamName: 'São Paulo',
-    },
-    teamAway: {
-      teamName: 'Internacional'
-    }
-  }]
-
-  it('se o corpo de times é um array de objetos', async () => {
-    const response = await chai.request(app).get('/matches');
-
-    expect(response.status).to.be.equal(200);
-    expect(response.body).to.be.a('array')
-  });
-});
