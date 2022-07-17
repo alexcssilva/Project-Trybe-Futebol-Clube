d7b8dc7cae138a62d0df06dfd36c654f26ec5e47
Alex Silva <alexcssilva@gmail.com>
2022-07-17 17:06:32 -0300

    feat: update match.protocol updatePatch - Desafio 24

--- app/backend/src/protocols/match.protocol.ts
+++ app/backend/src/protocols/match.protocol.ts
@@ -10,10 +10,12 @@ export interface IMatch {
 export interface IMatchModel {
   findAll(inProgress: boolean | null): Promise<IMatch[] | null>;
   createMatch(match: IMatch): Promise<IMatch>;
+  updatePatch(id: string): Promise<unknown>;
 };
 
 export interface IMatchService {
   findAll(inProgress: boolean | null): Promise<IMatch[] | null>;
   updateMatch(match: IMatch): Promise<IMatch>;
+  updatePatch(id: string): Promise<unknown>;
 };
 
