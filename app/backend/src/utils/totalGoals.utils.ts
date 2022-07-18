7a70e4c1d576f95692f22243037245be702d7844
Alex Silva <alexcssilva@gmail.com>
2022-07-18 18:50:53 -0300

    fix: update totalGoals

--- app/backend/src/utils/totalGoals.utils.ts
+++ app/backend/src/utils/totalGoals.utils.ts
@@ -1,9 +1,10 @@
 function totalGoals(victories: number, draws: number, losses: number) {
   const totalGames = victories + draws + losses;
-  const totalPoints = victories + 3 + draws * 1;
-  const efficiency = Math.round((totalPoints * 10000) / (totalGames * 3)) / 100;
+  const totalPoints = victories * 3 + draws * 1;
+  const efficiency = Math.round((totalPoints * 10000) / (totalGames * 3) / 100);
 
   return { totalGames, totalPoints, efficiency };
 }
 
 export default totalGoals;
+// Auxiliado por Rafael Jesus - Turma 17
