27f607c2df0287285c685601d7f3a0823f94ac31
Alex Silva <alexcssilva@gmail.com>
2022-07-18 19:00:14 -0300

    fix: update validateJWT

--- app/backend/src/middleware/validateJWT.ts
+++ app/backend/src/middleware/validateJWT.ts
@@ -4,15 +4,15 @@ import * as jwt from 'jsonwebtoken';
 
 const validateJWT = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization as string;
-  const JWT: string = process.env.JWT_SECRET || 'jwt_secrete';
-  jwt.verify(token, JWT) as 'jwt.JwtPayload';
-    try {
+  const JWT: string = process.env.JWT_SECRET || 'jwt_secret';
+  try {
+    jwt.verify(token, JWT) as 'jwt.JwtPayload';
 
-      next();
-    } catch (error) {
-      
-      return res.status(401).json({ message: 'Token must be a valid token' });
-    }
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Token must be a valid token' });
+  }
 };
 
 export default validateJWT;
+// Auxiliado por Rafael Jesus - Turma 17
